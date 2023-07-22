490cd8bea7cf5bff3f9054238d9ca8865a00e28d
Dmytro Frolov <dmitry.conquer@gmail.com>
2023-07-22 20:38:17 +0300

    update

--- components/Section/SectionSponsors.vue
+++ components/Section/SectionSponsors.vue
@@ -1,28 +1,28 @@
 <template>
-  <section class="mb-32 flex justify-center flex-wrap gap-x-16 gap-y-12 px-4">
+  <section class="mb-32 flex flex-wrap justify-center gap-x-16 gap-y-12 px-4">
     <img
       v-for="s in sponsors"
       :key="s"
       :src="s"
       :alt="s"
-      class="aspect-video object-contain sponsor" />
+      class="sponsor aspect-video object-contain" />
   </section>
 </template>
 
 <script setup>
-const sponsors = [
-  "/_nuxt/assets/images/sponsores/1.png",
-  "/_nuxt/assets/images/sponsores/2.png",
-  "/_nuxt/assets/images/sponsores/3.png",
-  "/_nuxt/assets/images/sponsores/4.png",
-  "/_nuxt/assets/images/sponsores/5.png",
-  "/_nuxt/assets/images/sponsores/6.png",
-  "/_nuxt/assets/images/sponsores/7.png",
-  "/_nuxt/assets/images/sponsores/8.png",
-];
+import spon1 from "~/assets/images/sponsores/1.png";
+import spon2 from "~/assets/images/sponsores/2.png";
+import spon3 from "~/assets/images/sponsores/3.png";
+import spon4 from "~/assets/images/sponsores/4.png";
+import spon5 from "~/assets/images/sponsores/5.png";
+import spon6 from "~/assets/images/sponsores/6.png";
+import spon7 from "~/assets/images/sponsores/7.png";
+import spon8 from "~/assets/images/sponsores/8.png";
+
+const sponsors = [spon1, spon2, spon3, spon4, spon5, spon6, spon7, spon8];
 
 onMounted(() => {
-  const {gsap} = useGsap();
+  const { gsap } = useGsap();
 
   const sponsor = gsap.timeline({
     scrollTrigger: {
@@ -39,7 +39,7 @@ onMounted(() => {
     },
     {
       y: 0,
-      stagger: 0.2
+      stagger: 0.2,
     },
   );
 });
