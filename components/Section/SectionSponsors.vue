bc2311e3f2df5d19ffcda6f252f1e76cb09c6617
Dmytro Frolov <dmitry.conquer@gmail.com>
2023-07-22 20:39:43 +0300

    update

--- components/Section/SectionSponsors.vue
+++ components/Section/SectionSponsors.vue
@@ -10,14 +10,14 @@
 </template>
 
 <script setup>
-import spon1 from "~/assets/images/sponsores/1.png";
-import spon2 from "~/assets/images/sponsores/2.png";
-import spon3 from "~/assets/images/sponsores/3.png";
-import spon4 from "~/assets/images/sponsores/4.png";
-import spon5 from "~/assets/images/sponsores/5.png";
-import spon6 from "~/assets/images/sponsores/6.png";
-import spon7 from "~/assets/images/sponsores/7.png";
-import spon8 from "~/assets/images/sponsores/8.png";
+import spon1 from "~/assets/images/sponsores/spon1.png";
+import spon2 from "~/assets/images/sponsores/spon2.png";
+import spon3 from "~/assets/images/sponsores/spon3.png";
+import spon4 from "~/assets/images/sponsores/spon4.png";
+import spon5 from "~/assets/images/sponsores/spon5.png";
+import spon6 from "~/assets/images/sponsores/spon6.png";
+import spon7 from "~/assets/images/sponsores/spon7.png";
+import spon8 from "~/assets/images/sponsores/spon8.png";
 
 const sponsors = [spon1, spon2, spon3, spon4, spon5, spon6, spon7, spon8];
 
